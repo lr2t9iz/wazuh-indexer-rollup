1350dd9cc5baf70bf0b60156dd16f20582fe1502
lr2t9iz <mitzepx01@gmail.com>
2023-10-29 16:02:53 -0600

    chore: datetype, timeout

--- rollup_app.py
+++ rollup_app.py
@@ -11,7 +11,7 @@ import json
 def post_data(creds, params, data_agg):
   with OpenSearch(hosts = [{'host': creds['WI_HOST'], 'port': 9200}],
     http_compress = True, http_auth = (creds['WI_USER'], creds['WI_PASS']),
-    use_ssl = True, verify_certs = False, ssl_show_warn = False) as client:
+    use_ssl = True, verify_certs = False, ssl_show_warn = False, timeout=300) as client:
     aux_control=1
     data2bulk=""
     now=time.strftime("%Y.%m.%d", time.localtime())
@@ -21,7 +21,7 @@ def post_data(creds, params, data_agg):
       event.update(params['set_col'])
       data2index = f"\n{json.dumps(index_header)}\n{json.dumps(event)}"
       data2bulk+=data2index
-      if aux_control % 5 == 0:
+      if aux_control % 1000 == 0:
         client.bulk(data2bulk)
         data2bulk=""
         time.sleep(1)
@@ -29,24 +29,27 @@ def post_data(creds, params, data_agg):
 
 def agg_data(data_json, cfg_name, params):
   df = pd.json_normalize(data_json, max_level=10)
-  df.drop(columns=["_index", "_id", "_score"], inplace=True)
+  df.drop(columns=["_index", "_id", "_type", "_score"], inplace=True) # 4.3.6
   df.columns = [col[8::] for col in df.columns]
   df['@timestamp'] = pd.to_datetime(df['@timestamp'], utc=True)
   df['@timestamp'] = df['@timestamp'].dt.floor(f"{params['dt_hist']}T")
   df.fillna('NA', inplace=True)
+  df = df.reindex(columns=params['fields'])
+  for cl in df.columns[1:]:
+    df = df.astype({cl:'string'})
   df = df.groupby(params['fields']).size().reset_index(name="count")
   df['@timestamp'] = df['@timestamp'].dt.strftime("%Y-%m-%dT%H:%M:%S")
   df.columns = [c.replace(".", "_") for c in df.columns]
   return  df.to_dict(orient='records')
 
 def get_data(creds, params):
-  db_docs_limit=500000 # set docs limit
+  db_docs_limit=600000 # set docs limit
   requests.put(f"https://{creds['WI_HOST']}:9200/{params['index_pattern']}/_settings",
     auth=requests.auth.HTTPBasicAuth(creds['WI_USER'], creds['WI_PASS']), verify=False,
     json={"index":{"max_result_window":db_docs_limit}})
   with OpenSearch(hosts = [{'host': creds['WI_HOST'], 'port': 9200}],
     http_compress = True, http_auth = (creds['WI_USER'], creds['WI_PASS']),
-    use_ssl = True, verify_certs = False, ssl_show_warn = False) as client:
+    use_ssl = True, verify_certs = False, ssl_show_warn = False, timeout=300) as client:
     query = {
       "size": db_docs_limit,
       "_source": { "includes": params['fields'] },
