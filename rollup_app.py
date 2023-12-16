0cc1ba6235b79ff0da9a180df9cb68e4baea5f3d
lr2t9iz <mitzepx01@gmail.com>
2023-12-15 18:40:09 -0600

    chore index_name

--- rollup_app.py
+++ rollup_app.py
@@ -10,15 +10,14 @@ import time
 import json
 import sys
 
-def post_data(creds, params, data_agg):
+def post_data(creds, params, data_agg, since_fdatei):
     with OpenSearch(hosts = [{'host': creds['WI_HOST'], 'port': 9200}],
         http_compress = True, http_auth = (creds['WI_USER'], creds['WI_PASS']),
         use_ssl = True, verify_certs = False, ssl_show_warn = False, timeout=300) as client:
         aux_control=1
         data2bulk=""
-        now=time.strftime("%Y.%m.%d", time.localtime())
         for event in data_agg:
-            index_header = { "index" : { "_index": f"wazuh-agg-{now}" } }
+            index_header = { "index" : { "_index": f"wazuh-agg-{since_fdatei}" } }
             event['tag'] = params['add_tag']
             event.update(params['set_col'])
             data2index = f"\n{json.dumps(index_header)}\n{json.dumps(event)}"
@@ -56,7 +55,7 @@ def get_data(creds, params, at, zt):
         query = {
             "size": db_docs_limit,
             "_source": { "includes": params['fields'] },
-            "query": { "bool": { "filter": [ { "range": {"@timestamp": { "gte": at, "lt": zt } } }, params['filter'] ] } }
+            "query": { "bool": { "filter": [ { "range": {"@timestamp": { "gte": at, "lte": zt } } }, params['filter'] ] } }
         }
         response = client.search(index=params['index_pattern'], body=query)['hits']['hits']
         return response
@@ -68,19 +67,22 @@ def main(configs="rollup.yml"):
         confs = yaml.safe_load(yamlfile)
     for cfg_name, params in confs.items():
         # until_ndays_ago
-        spec = "%Y-%m-%d"
         since_date = datetime.today()-timedelta(days=params['until_ndays_ago'])
-        since_fdate = f"{str(since_date.strftime(spec))}"
+        since_fdate = f"{str(since_date.strftime("%Y-%m-%d"))}"
+        since_fdatei = f"{str(since_date.strftime("%Y.%m.%d"))}"
         # get, agg & post events by hour
         for i in range(0,24):
-            time.sleet(30)
+            time.sleep(30)
             hh="{:02d}".format(i)
             at = f"{since_fdate}T{hh}:00:00.000Z"
             zt = f"{since_fdate}T{hh}:59:59.999Z"
+            #print("get data")
             data_json = get_data(creds, params, at, zt)
             if len(data_json):
+                #print("agg data")
                 data_agg = agg_data(data_json, cfg_name, params)
-                post_data(creds, params, data_agg)
+                #print("post data")
+                post_data(creds, params, data_agg, since_fdatei)
         # delete event agg-ed
 
 if __name__=="__main__":
